0eca90afcd2da355f785dd4d5b74bf399f1fc03a
Jeenal-Darji <jeenal.darji22@gmail.com>
2024-09-08 11:25:30 +0530

    Add files via upload

---
+++ frontend_components/LoginForm.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './LoginForm.css'; // Import CSS file for styling
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const [error, setError] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+  
+      if (response.ok) {
+        console.log('Login successful');
+        navigate('/Booking'); // Navigate to home page
+        setError(''); // Clear the error message
+      } else {
+        if (response.status === 401) {
+          setError('Invalid email or password');
+        } else if (response.status === 400) {
+          setError('Bad request');
+        } else if (response.status === 500) {
+          setError('Internal server error');
+        } else {
+          setError('An unexpected error occurred');
+        }
+      }
+    } catch (error) {
+      console.error('Error occurred during login:', error);
+      setError('An unexpected error occurred');
+    }
+  };
+  
+  return (
+    <div className="login-container">
+      <h2 className="login-title">Login</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="form-input"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="form-label">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="form-input"
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {error && <p className="error-message">{error}</p>}
+    </div>
+  );
+};
+
+export default LoginForm;
